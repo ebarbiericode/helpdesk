b60f47e8166009841a83004b6ca22695af45488b
Elisandro <elisandro.barbieri@gmail.com>
2019-04-05 16:23:20 -0300

    link to the home page

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
     <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="home.php">
       <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       App Help Desk
     </a>
     <ul class="navbar-nav">
       <li class="nav-item"> 
-        <a class="nav-link" href="logoff.php">Sair</a>
+        <a class="nav-link" href="logoff.php">Logoff</a>
       </li>
     </ul>
   </nav>
