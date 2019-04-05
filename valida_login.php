3366120d89e709af343b9cf1a3d1777088337520
Elisandro <elisandro.barbieri@gmail.com>
2019-04-05 15:32:25 -0300

    validate login

--- valida_login.php
+++ valida_login.php
@@ -5,11 +5,16 @@
 
 	//variavel que verifica se a autenticacao foi realizada
 	$usuario_autenticado = false;
+	$usuario_id = null;
+	$usuario_perfil_id = null;
+	$perfis = array(1 => 'Administrativo', 2 => 'Usuário');
 
-	//usuarios do sistema
+	//usuarios do sistema - ID, Email, Senha, Permissão (1 - adm, 2 - usuario)
 	$usuarios_app = array(
-		array('email' => 'adm@teste.com.br', 'senha' => '12345'),
-		array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+		array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '12345', 'perfil_perm' => 1),
+		array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '12345', 'perfil_perm' => 1),
+		array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '12345', 'perfil_perm' => 2),
+		array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '12345', 'perfil_perm' => 2),
 	);
 
 	// echo '<pre>';
@@ -20,6 +25,8 @@
 
 		if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
 			$usuario_autenticado = true;
+			$usuario_id = $user['id'];
+			$usuario_permissao = $user['perfil_perm'];
 		}
 
 	}
@@ -27,6 +34,8 @@
 	if($usuario_autenticado){
 		echo 'Usuário autenticado';
 		$_SESSION['autenticado'] = 'SIM';
+		$_SESSION['id'] = $usuario_id;
+		$_SESSION['perfil_perm'] = $usuario_permissao;
 		header('Location: home.php');
 	} else {
 		$_SESSION['autenticado'] = 'NAO';
